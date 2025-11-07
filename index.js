311b166b47cc2ee3d927bde998ce99272e6e0246
Nicollas Lott <nicollaslottcurvellano@outlook.com>
2025-11-06 22:51:48 -0300

    Atualizadas as mensagens do sistema

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ import { turboLogger } from './turboLogger.js';
 console.log('Estou usando GitHub');
 
 turboLogger('Estou aprendendo o fluxo do git', 'orange');
+turboLogger('Eu consegui criar uma nova mensagem sem afetar a versão principal do sistema!', 'darkgreen');
